61861b7b2ec2bb778dbb03bde1f9ae1b9b04f0fe
Jack Case <jackacase@gmail.com>
2018-03-13 20:46:52 -0400

    added mongoDB connection

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Set up the mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'localhost:27017';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
