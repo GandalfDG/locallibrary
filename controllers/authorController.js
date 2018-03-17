93092bf8f63de3081cc91456fd8bbeeb496e76b7
Jack Case <jackacase@gmail.com>
2018-03-17 11:28:38 -0400

    author and genre views

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,7 +2,10 @@ var Author = require('../models/author');
 
 //Display list of all authors
 exports.author_list = function(req, res) {
-    rs.send('NOT IMPLEMENTED: Author list');
+    Author.find().sort([['family_name', 'ascending']]).exec(function (err, list_authors) {
+        if(err) { return next(err); }
+        res.render('author_list', {title: 'Author List', author_list: list_authors});
+    });
 };
 
 //Display detail page for a specific author
